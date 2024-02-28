086e777945925bae2febed930c5064eea1286f94
Marcin Panek <evilmeryl@gmail.com>
2024-02-28 21:16:26 +0100

    Models
    fmovies and accounts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from backend.local_setings import DATABASES_INFO
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,10 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'corsheaders',
-
     'accounts',
     'mainapp'
 ]
@@ -120,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128, blank=False)
+    desc = models.models.TextField(blank=False)
+    date = models.DateField(default=True)
+    imag = models.ImageField(upload_to="images/")
+    duration = models.IntegerField()
+    rate = models.FloatField()
+    author = models.CharField(max_length= 64)
